18a8505fd2c5bb92f308a7961591feb749e9a31d
SohamDesai1 <sohamdesai123@gmail.com>
2025-05-12 18:38:02 +0530

    feat(frontend): :sparkles: initialize supabase

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:frontend/core/router/app_router.dart';
 import 'package:sizer/sizer.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   const isProd = !kDebugMode;
   await dotenv.load(fileName: isProd ? ".env.prod" : ".env.dev");
+  await Supabase.initialize(
+      url: dotenv.env['SUPABASE_PROJECT_URL']!,
+      anonKey: dotenv.env['SUPABASE_ANNON_KEY']!);
   runApp(const MainApp());
 }
 
@@ -20,6 +24,7 @@ class MainApp extends StatelessWidget {
         return MaterialApp.router(
           theme: ThemeData(
             scaffoldBackgroundColor: const Color(0xFF271F24),
+            fontFamily: 'Formula1Regular',
             appBarTheme: AppBarTheme(
                 backgroundColor: Theme.of(context).scaffoldBackgroundColor,
                 toolbarHeight: 80,
