c65be474b4eeb1f11e97d6fe62d97f987fc1c0fd
SohamDesai1 <sohamdesai123@gmail.com>
2025-11-09 02:17:46 +0530

    feat(frontend): :sparkles: added bottom bar

--- lib/presentation/home/views/home_screen.dart
+++ lib/presentation/home/views/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/presentation/home/cubit/bottom_bar_cubit.dart';
+import 'package:frontend/widgets/bottom_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -7,13 +10,45 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeState();
 }
 
-class _HomeState extends State<HomeScreen> {
+class _HomeState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Home'),
-      ),
+    return BlocBuilder<NavigationCubit, int>(
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            title: const Text('F1 Hub',
+                style: TextStyle(
+                    fontFamily: "Formula1Regular", color: Colors.white)),
+            centerTitle: true,
+          ),
+          bottomNavigationBar: BottomNavBar(),
+          body: IndexedStack(
+            index: state,
+            children: const [
+              Center(child: Text('Home Screen')),
+              Center(child: Text('Standings Screen')),
+              Center(child: Text('Drivers Screen')),
+              Center(child: Text('Settings Screen')),
+            ],
+          ),
+        );
+      },
     );
   }
 }
