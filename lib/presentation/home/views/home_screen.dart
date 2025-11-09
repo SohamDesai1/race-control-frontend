67c9a84f1b5846a2731f606895671c0f6b24d561
SohamDesai1 <sohamdesai123@gmail.com>
2025-11-09 15:09:29 +0530

    feat(frontend): :sparkles: added widgets

--- lib/presentation/home/views/home_screen.dart
+++ lib/presentation/home/views/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend/presentation/home/cubit/bottom_bar_cubit.dart';
+import 'package:frontend/presentation/home/views/dashboard_screen.dart';
 import 'package:frontend/widgets/bottom_bar.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -27,26 +28,24 @@ class _HomeState extends State<HomeScreen> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+    List screens = [
+      const DashboardScreen(),
+      const Center(child: Text('Standings Screen')),
+      const Center(child: Text('Drivers Screen')),
+      const Center(child: Text('Settings Screen')),
+    ];
     return BlocBuilder<NavigationCubit, int>(
-      builder: (context, state) {
+      builder: (context, index) {
         return Scaffold(
           appBar: AppBar(
             backgroundColor: Theme.of(context).scaffoldBackgroundColor,
             title: const Text('F1 Hub',
-                style: TextStyle(
-                    fontFamily: "Formula1Regular", color: Colors.white)),
+                style:
+                    TextStyle(fontFamily: "Formula1Bold", color: Colors.white)),
             centerTitle: true,
           ),
           bottomNavigationBar: BottomNavBar(),
-          body: IndexedStack(
-            index: state,
-            children: const [
-              Center(child: Text('Home Screen')),
-              Center(child: Text('Standings Screen')),
-              Center(child: Text('Drivers Screen')),
-              Center(child: Text('Settings Screen')),
-            ],
-          ),
+          body: screens[index],
         );
       },
     );
