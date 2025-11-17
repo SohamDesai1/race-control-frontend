89ff8d2cd95f62c4d0fb9f0e6a0fa5d51cb72aed
SohamDesai1 <sohamdesai123@gmail.com>
2025-11-17 21:13:01 +0530

    feat(frontend): :sparkles: added model for upcoming race

---
+++ lib/models/upcoming_race.dart
@@ -0,0 +1,20 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'upcoming_race.freezed.dart';
+part 'upcoming_race.g.dart';
+
+@freezed
+abstract class UpcomingRaceModel with _$UpcomingRaceModel {
+  factory UpcomingRaceModel({
+    required String circuitId,
+    required DateTime createdAt,
+    required DateTime date,
+    required int id,
+    required String raceName,
+    required String round,
+    required String season,
+    required String time,
+  }) = _UpcomingRaceModel;
+
+  factory UpcomingRaceModel.fromJson(Map<String, dynamic> json) =>
+      _$UpcomingRaceModelFromJson(json);
+}
