d0f02f0192c4f1f90e47695653e7689c18dbb297
SohamDesai1 <sohamdesai123@gmail.com>
2025-11-20 23:19:50 +0530

    feat(frontend): :sparkles: added cubit

---
+++ lib/presentation/home/cubit/dashboard/dashboard_cubit.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+import '../../../../repositories/race_repository.dart';
+import 'package:meta/meta.dart';
+part 'dashboard_state.dart';
+
+
+@injectable
+class DashboardCubit extends Cubit<DashboardState> {
+  DashboardCubit(this.raceRepository) : super(DashboardInitial());
+
+  final RaceRepository raceRepository;
+
+  Future<void> fetchUpcomingRaces() async {
+    try {
+      emit(DashboardLoading());
+      final result = await raceRepository.getUpcomingRaces();
+      result.fold(
+        (failure) => emit(DashboardError(failure.message)),
+        (races) => emit(DashboardSuccess(races)),
+      );
+    } catch (e) {
+      emit(DashboardError(e.toString()));
+    }
+  }
+}
---
+++ lib/presentation/home/cubit/dashboard/dashboard_state.dart
@@ -0,0 +1,20 @@
+part of 'dashboard_cubit.dart';
+
+@immutable
+abstract class DashboardState {}
+
+class DashboardInitial extends DashboardState {}
+
+class DashboardLoading extends DashboardState {}
+
+class DashboardSuccess extends DashboardState {
+  final List<dynamic> races;
+
+  DashboardSuccess(this.races);
+}
+
+class DashboardError extends DashboardState {
+  final String message;
+
+  DashboardError(this.message);
+}
