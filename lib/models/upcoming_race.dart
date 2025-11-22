f228b63a5615ce2f1f55fa9d7b73aa5361f47842
SohamDesai1 <sohamdesai123@gmail.com>
2025-11-23 01:02:26 +0530

    feat(frontend): :zap: updated model

--- lib/models/upcoming_race.dart
+++ lib/models/upcoming_race.dart
@@ -6,12 +6,14 @@ part 'upcoming_race.g.dart';
 abstract class UpcomingRaceModel with _$UpcomingRaceModel {
   factory UpcomingRaceModel({
     required String circuitId,
-    required DateTime createdAt,
+    @JsonKey(name: 'created_at') 
+    required String createdAt,
     required String date,
     required int id,
     required String raceName,
     required String round,
     required String season,
+
     required String time,
   }) = _UpcomingRaceModel;
 
