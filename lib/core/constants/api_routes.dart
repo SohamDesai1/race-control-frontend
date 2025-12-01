93b31639f990298915d5f8530f05115aaadee618
SohamDesai1 <sohamdesai123@gmail.com>
2025-12-01 13:28:45 +0530

    feat(frontend): :alien: added refresh token route

--- lib/core/constants/api_routes.dart
+++ lib/core/constants/api_routes.dart
@@ -1,6 +1,7 @@
 class ApiRoutes {
   static const String login = '/auth/login';
   static const String register = '/auth/register';
+  static const String refreshToken = '/auth/refresh';
   static const String upcomingRaces = '/race/get_upcoming_race_data';
   static const String recentResults = '/race/get_race_results';
   static String driverLeaderboard(int year) => '/standings/driver_standings/$year';
